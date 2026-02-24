9b2390fa318936ed97ea69c91fec20d1f6b4fd5c
glmck13 <glmck13@verizon.net>
2026-02-24 15:22:55 -0500

    Update a2g.sh

--- Google-Gemini/a2g.sh
+++ Google-Gemini/a2g.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-source $HOME/a2genv/bin/activate
-
-cd $HOME/local
-
-. ./geminicreds.sh
+cd $HOME/local/a2g
+source ./a2genv/bin/activate
+. ./env.sh
 
 ./a2g.py >/tmp/a2g.log 2>&1 &
